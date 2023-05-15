7fee89df63b91ab933784a8d78b062ada3ebaefa
Jonny Brownrigg <jonny-uk@hotmail.com>
2023-05-15 15:56:25 +0100

    committing before merge

---
+++ Modules/doubly_linked_class.py
@@ -0,0 +1,22 @@
+"""
+Douely linked list class definition
+"""
+
+
+class Node():
+    """Node class with a next and previous pointer to add to doubly linked list"""
+    
+    def __init__(self, value):
+        self.value = value
+        self.next_node = None
+        self.previous_node = None
+
+
+class DoublyLinkedList:
+    """Doubly linked list class and associated methods."""
+
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
--- Modules/linked_list_class.py
+++ Modules/linked_list_class.py
@@ -1,4 +1,5 @@
 """
+Contains the linked list class
 """
 
 
