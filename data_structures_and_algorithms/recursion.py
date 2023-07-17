1aef002f51a0bb63d8a4c52fb4019bf7d267f701
JonnySB <jonny-uk@hotmail.com>
2023-07-17 11:17:33 +0100

    adding recursion section

---
+++ data_structures_and_algorithms/recursion.py
@@ -0,0 +1,13 @@
+def factorial(n):
+    if n == 1:
+        return 1
+    return n * factorial(n-1)
+
+def pyramid(n):
+    if n == 1:
+        return 1
+    return n + pyramid(n-1)
+
+print(factorial(5))
+print(pyramid(10))
+
