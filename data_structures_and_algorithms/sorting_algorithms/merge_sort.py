fd57daf3261e3bb37d25dee4fb2528c1480829f9
JonnySB <jonny-uk@hotmail.com>
2023-07-28 22:34:13 +0100

    added merge sort for lists code

--- data_structures_and_algorithms/sorting_algorithms/merge_sort.py
+++ data_structures_and_algorithms/sorting_algorithms/merge_sort.py
@@ -2,29 +2,31 @@
 
 # Merge function - helper function to combine two sorted list
 def merge(arr1: list, arr2: list):
-    arr3 = []
+    combined = []
     i = 0
     j = 0
     while i < len(arr1) and j < len(arr2):
         if arr1[i] < arr2[j]:
-            arr3.append(arr1[i])
+            combined.append(arr1[i])
             i += 1
         else:
-            arr3.append(arr2[j])
+            combined.append(arr2[j])
             j += 1
-    if i < len(arr1):
-        arr3.extend(arr1[i:])
-    else:
-        arr3.extend(arr2[j:])
-    return arr3
+    while i < len(arr1):
+        combined.append(arr1[i])
+        i += 1
+    while j < len(arr2):
+        combined.append(arr2[j])
+        j += 1
+    return combined
 
+def merge_sort(arr):
+    if len(arr) == 1:
+        return arr
+    midpoint = len(arr) // 2
+    left = merge_sort(arr[:midpoint])
+    right = merge_sort(arr[midpoint:])
 
+    return merge(left,right)
 
-
-
-
-
-arr1 = []
-arr2 = [2,4,7,8]
-
-print(merge(arr1,arr2))
+print(merge_sort([8,5,3,2,5]))
