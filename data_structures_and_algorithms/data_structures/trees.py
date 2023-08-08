5bd43b7d3486e0158f780aa35e7a2ddbf10cf4ee
jonny <jonny-uk@hotmail.com>
2023-08-08 08:43:12 +0100

    tree traversal - depth first search, 3 methods

--- data_structures_and_algorithms/data_structures/trees.py
+++ data_structures_and_algorithms/data_structures/trees.py
@@ -56,6 +56,39 @@ class BinarySearchTree:
                 queue.append(current_node.right)
         return results
 
+    def pre_order_dfs(self):
+        results = []
+        def traverse(current_node):
+            results.append(current_node.value)
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
+        traverse(self.root)
+        return results
+
+    def post_order_dfs(self):
+        results = []
+        def traverse(current_node):
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
+            results.append(current_node.value)
+        traverse(self.root)
+        return results
+
+    def in_order_dfs(self):
+        results = []
+        def traverse(current_node):
+            if current_node.left is not None:
+                traverse(current_node.left)
+            results.append(current_node.value)
+            if current_node.right is not None:
+                traverse(current_node.right)
+        traverse(self.root)
+        return results
+
 
 my_tree = BinarySearchTree()
 my_tree.insert(47)
@@ -66,4 +99,6 @@ my_tree.insert(27)
 my_tree.insert(52)
 my_tree.insert(82)
 
-print(my_tree.BFS())
+print(my_tree.pre_order_dfs())
+print(my_tree.post_order_dfs())
+print(my_tree.in_order_dfs())
