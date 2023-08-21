819deebd4d6de4eeab57461abd5a5c3ca488502f
jonny <jonny-uk@hotmail.com>
2023-08-21 08:59:14 +0100

    adding final list questions

--- data_structures_and_algorithms/interview_style_questions/trees/kth_smallest_node.py
+++ data_structures_and_algorithms/interview_style_questions/trees/kth_smallest_node.py
@@ -6,7 +6,7 @@ The solution to this problem usually involves traversing the tree in-order (left
 
 Iterative approach using a stack: This approach involves maintaining a stack of nodes that still need to be visited, starting with the leftmost node. At each step, you pop a node off the stack, decrement the kth smallest counter, and check whether you have found the kth smallest element. If you have not, you continue traversing the tree by moving to the right child of the current node.
 
-Recursive approach: This approach involves recursively traversing the tree in-order and keeping track of the number of nodes visited until you find the kth smallest element. You can use a helper function that takes a node and a value of k as input, and recursively calls itself on the left and right children of the node until it finds the kth smallest element.
+ecursive approach: This approach involves recursively traversing the tree in-order and keeping track of the number of nodes visited until you find the kth smallest element. You can use a helper function that takes a node and a value of k as input, and recursively calls itself on the left and right children of the node until it finds the kth smallest element.
 
 Both of these approaches have their own advantages and disadvantages, and the best approach to use may depend on the specific problem constraints and the interviewer's preferences.
 ''' 
@@ -78,6 +78,27 @@ class BinarySearchTree:
 
         return None
 
+    def kth_smallest(self, k):
+        self.kth_smallest_count = 0
+        return self.kth_smallest_helper(self.root, k)
+ 
+    def kth_smallest_helper(self, node, k):
+        if node is None:
+            return None
+ 
+        left_result = self.kth_smallest_helper(node.left, k)
+        if left_result is not None:
+            return left_result
+ 
+        self.kth_smallest_count += 1
+        if self.kth_smallest_count == k:
+            return node.value
+ 
+        right_result = self.kth_smallest_helper(node.right, k)
+        if right_result is not None:
+            return right_result
+ 
+        return None
 
 
 
@@ -91,6 +112,8 @@ bst.insert(2)
 bst.insert(4)
 bst.insert(6)
 bst.insert(8)
+
+print(bst.root.value)
 
 print(bst.kth_smallest(1))  # Expected output: 2
 print(bst.kth_smallest(3))  # Expected output: 4
