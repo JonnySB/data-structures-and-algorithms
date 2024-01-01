947a248a55be9d9775da2dbfdf1b63137be2e360
Jonny Brownrigg <jonny-uk@hotmail.com>
2024-01-01 19:16:55 +0000

    completed containsDuplicates

---
+++ leetcode/arrays_and_hashing/mergeAlternate.py
@@ -0,0 +1,12 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        output_list = []
+        min_length = min(len(word1), len(word2))
+
+        for i in range(min_length):
+            output_list.append(word1[i])
+            output_list.append(word2[i])
+
+        output_list.extend([word1[min_length:], word2[min_length:]])
+
+        return "".join(output_list)
