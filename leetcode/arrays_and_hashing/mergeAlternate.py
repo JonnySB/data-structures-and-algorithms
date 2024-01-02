ff7c896a91a663fa1557abc8f7d434e09031b36c
Jonny Brownrigg <jonny-uk@hotmail.com>
2024-01-02 13:07:20 +0000

    updated formatting

--- leetcode/arrays_and_hashing/mergeAlternate.py
+++ leetcode/arrays_and_hashing/mergeAlternate.py
@@ -1,12 +1,11 @@
-class Solution:
-    def mergeAlternately(self, word1: str, word2: str) -> str:
-        output_list = []
-        min_length = min(len(word1), len(word2))
+def mergeAlternately(word1: str, word2: str) -> str:
+    output_list = []
+    min_length = min(len(word1), len(word2))
 
-        for i in range(min_length):
-            output_list.append(word1[i])
-            output_list.append(word2[i])
+    for i in range(min_length):
+        output_list.append(word1[i])
+        output_list.append(word2[i])
 
-        output_list.extend([word1[min_length:], word2[min_length:]])
+    output_list.extend([word1[min_length:], word2[min_length:]])
 
-        return "".join(output_list)
+    return "".join(output_list)
