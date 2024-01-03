a44b73bb9e40130c3e8ccb93fecada704d20347a
Jonny Brownrigg <jonny-uk@hotmail.com>
2024-01-03 11:37:58 +0000

    encodeAndDecodeStrings

--- leetcode/arrays_and_hashing/encodeAndDecodeStrings.py
+++ leetcode/arrays_and_hashing/encodeAndDecodeStrings.py
@@ -18,8 +18,31 @@ Explanation:
 One possible encode method is: "we:;say:;:::;yes"
 """
 
-def encode(strs: list[str]) -> str:
-    pass
 
-def decode(data: str]) -> list(str):
-    pass
+def encode(strs: list[str]) -> str:
+    res = ""
+    for s in strs:
+        res += str(len(s)) + "#" + s
+    return res
+
+
+def decode(s: str) -> list[str]:
+    res = []
+    i = 0
+    while i < len(s):
+        j = i
+        while s[j] != "#":
+            j += 1
+        length = int(s[i:j])
+        start = j + 1
+        end = start + length
+        res.append(s[start:end])
+        i = end
+    return res
+
+
+encoded_string = encode(["lint", "code", "love", "you"])
+print(encoded_string)
+
+decoded_string = decode(encoded_string)
+print(decoded_string)
--- leetcode/arrays_and_hashing/mergeAlternate.py
+++ leetcode/arrays_and_hashing/mergeAlternate.py
@@ -9,3 +9,22 @@ def mergeAlternately(word1: str, word2: str) -> str:
     output_list.extend([word1[min_length:], word2[min_length:]])
 
     return "".join(output_list)
+
+
+def mergeAlternatelyOther(word1: str, word2: str) -> str:
+    res = ""
+
+    word1_len = len(word1)
+    word2_len = len(word2)
+    max_len = max(word1_len, word2_len)
+
+    for i in range(max_len):
+        if i < word1_len:
+            res += word1[i]
+        if i < word2_len:
+            res += word2[i]
+
+    return res
+
+
+print(mergeAlternatelyOther("abc", "defgi"))
